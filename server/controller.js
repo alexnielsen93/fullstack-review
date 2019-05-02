b1d32aa1e17c958ddb3979864b4fdc1036715448
Alex Nielsen <alexnielsen@Alexs-MacBook-Pro-2.local>
2019-05-02 11:18:08 -0600

    first commit

--- server/controller.js
+++ server/controller.js
@@ -1,9 +1,28 @@
+const bcrypt = require('bcryptjs')
+
 module.exports ={
   getUsers: (req,res)=>{
     const db = req.app.get('db')
     db.getAllUsers().then((data)=>
     res.status(200).send(data)
-    )}
+    )},
+
+    register: async(req, res)=>{
+      const db = req.app.get('db')
+      const {email, firstname, lastname, username, password} = req.body
+      const { session } = req
+      let emailTaken = await(db.checkEmail({email}))
+      emailTaken = +emailTaken[0].count
+      if (emailTaken !=0){
+        return res.sendStatus(409)
+      }
+      const salt = bcrypt.genSaltSync(10)
+      //hashing a character is randomizing a password
+      //salt(2) is 2 rounds of randomization
+      const hash = bcrypt.hashSync(password, salt)      
+    },
+
+
 
 
 }
